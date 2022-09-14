e21c8ebe6c1c2b65fe5ffd50d93e5e7c21f72f5b
danieldamian09 <humbertorivero9@gmail.com>
2022-09-13 22:56:11 -0300

    Fix vista pokemons

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,33 @@
-import { useEffect, useState } from 'react'
-import { getPokemonsHome } from './services/getPokemonsHome'
+import {useEffect, useState} from "react";
+import {getPokemonsHome} from "./services/getPokemonsHome";
 
 function App() {
-
-  const [pokemonsHome, setPokemonsHome] = useState([])
+  const [pokemonsHome, setPokemonsHome] = useState([]);
+  // Paginacion
+  const [nextPage, setNextPage] = useState(0);
 
 
   useEffect(() => {
-    getPokemonsHome().then(pokemons => setPokemonsHome(pokemons))
-  }, [])
-  
+		getPokemonsHome(nextPage).then((pokemons) => setPokemonsHome(pokemons));
+	}, [nextPage]);
 
-  return (
-    <>
-      <div>
-        {pokemonsHome.map(pokemon => (
-          <div key={pokemon.name}>
-            <h2>{pokemon.name}</h2>
-            <img src={pokemon.image} alt={pokemon.name} width="200px" />
-          </div>
-          ))}
-      </div>
-    </>
-  )
+	return (
+		<div className="container mx-auto min-h-screen">
+			<h1 className="text-white text-3xl text-center py-2">Pokemons</h1>
+			<div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-8 gap-6">
+				{pokemonsHome.map((pokemon) => (
+					<div key={pokemon.name} className="border p-4">
+						<h2 className="text-white text-center py-3">{pokemon.name}</h2>
+					</div>
+				))}
+			</div>
+			{/* Paginacion */}
+			<div className="w-full flex justify-between py-6">
+				<button className="bg-blue-600 rounded-lg py-2 px-4 text-white" onClick={() => setNextPage(prev => prev - 1)} disabled={nextPage === 0 ? true :false}>Anterior</button>
+				<button className="bg-blue-600 rounded-lg py-2 px-4 text-white"onClick={() => setNextPage(prev => prev + 1)}>Siguiente</button>
+			</div>
+		</div>
+	);
 }
 
-export default App
+export default App;
--- src/services/getPokemonsHome.js
+++ src/services/getPokemonsHome.js
@@ -1,21 +1,9 @@
-export const getPokemonsHome = async () => {
-	const url = `https://pokeapi.co/api/v2/pokemon?limit=20&offset=0`;
+export const getPokemonsHome = async (page) => {
+	const url = `https://pokeapi.co/api/v2/pokemon?limit=10&offset=${page * 10}`;
 	try {
 		const response = await fetch(url);
     const results = await response.json();
-    console.log(results.results);
-
-		const pokemons = results.results.map((pokemon, index) => {
-			return {
-				id: index + 1,
-				name: pokemon.name,
-				image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${
-					index + 1
-				}.svg`,
-			};
-		});
-
-		return pokemons;
+    return results.results;
 	} catch (error) {
 		console.log(error);
 	}
